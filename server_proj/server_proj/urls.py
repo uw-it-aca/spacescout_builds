09079cc1f759b65abe7814f93d586e4c1635a278
Craig M. Stimmel <cstimmel@uw.edu>
2012-09-04 10:18:24 -0700

    Added urls.py appropriate for the server.

--- server_proj/server_proj/urls.py
+++ server_proj/server_proj/urls.py
@@ -1,17 +1,27 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'server_proj.views.home', name='home'),
-    # url(r'^server_proj/', include('server_proj.foo.urls')),
+    # url(r'^$', 'spotseeker.views.home', name='home'),
+    # url(r'^spotseeker/', include('spotseeker.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include('spotseeker_server.urls')),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
