5b37c8a526c259b4680142791e314d17faf65975
Craig M. Stimmel <cstimmel@uw.edu>
2012-10-04 14:17:56 -0700

    Added django-mobility for selecting mobile specific templates.

--- web_proj/web_proj/settings.py
+++ web_proj/web_proj/settings.py
@@ -101,6 +101,8 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'mobility.middleware.DetectMobileMiddleware',
+    'mobility.middleware.XMobileMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
