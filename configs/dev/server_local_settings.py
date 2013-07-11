0fed114d37cc4361de57a1ab22ae2bb4c37138f6
Craig M. Stimmel <cstimmel@uw.edu>
2013-07-11 15:20:03 -0700

    Set the correct EI Form settings, for reals this time.

--- configs/dev/server_local_settings.py
+++ configs/dev/server_local_settings.py
@@ -17,7 +17,7 @@ SPOTSEEKER_AUTH_MODULE = 'spotseeker_server.auth.all_ok'
 
 # Custom validation can be added by adding SpotForm and ExtendedInfoForm to org_forms and setting them here.
 #SPOTSEEKER_SPOT_FORM = 'spotseeker_server.org_forms.uw_spot.UWSpotForm'
-#SPOTSEEKER_EXTENDEDINFO_FORM = 'spotseeker_server.org_forms.uw_spot.UWSpotExtendedInfoForm'
+#SPOTSEEKER_SPOTEXTENDEDINFO_FORM = 'spotseeker_server.org_forms.uw_spot.UWSpotExtendedInfoForm'
 
 DATABASES = {
     'default': {
