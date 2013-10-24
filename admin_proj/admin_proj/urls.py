2f225ff69ddbf8dfa4049903ad070069d6923a6f
Craig M. Stimmel <cstimmel@uw.edu>
2013-10-24 10:35:48 -0700

    Quick fix to get to the django admin when the app itself is mounted at /admin.

--- admin_proj/admin_proj/urls.py
+++ admin_proj/admin_proj/urls.py
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^djangoadmin/', include(admin.site.urls)),
 
     url(r'^', include('spacescout_admin.urls')),
 )
