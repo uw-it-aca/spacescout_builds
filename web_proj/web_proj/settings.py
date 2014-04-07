ae01c9169adc83350124ef17b0780bff93af43f3
Patrick Michaud <pmichaud@uw.edu>
2014-04-07 14:24:26 -0700

    Forcing the static path to be random, so we don't get any more /static/ commits

--- web_proj/web_proj/settings.py
+++ web_proj/web_proj/settings.py
@@ -79,7 +79,8 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+import random
+STATIC_URL = '/static%s/' % random.random()
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
