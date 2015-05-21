3a397797eb782b122cf99622b01aaa2c977cf8ae
Kaiyu Zheng <qaz2wsx3@uw.edu>
2015-05-21 10:58:26 -0700

    Changing the logging config in labstats proj settings to keep it consistent with web proj's settings. This also fixes the bug: No handlers could be found for logger

--- labstats_proj/labstats_proj/settings.py
+++ labstats_proj/labstats_proj/settings.py
@@ -125,16 +125,31 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        },
+    },
     'handlers': {
         'mail_admins': {
             'level': 'ERROR',
+            'filters': ['require_debug_false'],
             'class': 'django.utils.log.AdminEmailHandler'
-        }
+        },
+        'console': {
+            'level': 'WARNING',
+            'class': 'logging.StreamHandler',
+        },
     },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
             'level': 'ERROR',
+            'propagate': True,
+        },
+        '': {
+            'handlers': ['console'],
+            'level': 'WARNING',
             'propagate': True,
         },
     }
